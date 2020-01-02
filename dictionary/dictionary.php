ad6ed39d9cc8324493ad0f2eaa097f3e86772bb4
internetgho5t <brandon_jordan360@yahoo.com>
2020-01-02 12:31:05 -0500

    Simplified dictionary solution

--- dictionary/dictionary.php
+++ dictionary/dictionary.php
@@ -1,9 +1,3 @@
 <?php
 // This script will create a javascript dictionary from the POST data instead of variables
-if($_POST) {
-  $vars = array();
-  foreach($_POST as $field => $value) {
-    array_push($vars,"$field:".json_encode($value)); // Push to $vars array so we can just implode() it, escape value
-  }
-  echo "<script>var post = {".implode(", ",$vars)."}</script>\n"; // Implode array, javascript will interpret as dictionary
-}
+if($_POST)echo "<script>var post = ".json_encode($_POST).";</script>\n"; // Put POST data into a javascript dictionary
